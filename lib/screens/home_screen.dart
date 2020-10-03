bd4c79d9b4bca16183e810385973bdbb3e21679e
Yuya Ohbayashi <yuya9445@gmail.com>
2020-10-03 16:54:35 +0900

    059

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,12 +9,29 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("おくすりアラート"),
-        centerTitle: true,
-      ),
-      drawer: Drawer(),
-      floatingActionButton: FloatingActionButton(),
-    );
+        appBar: AppBar(
+          title: Text("おくすりアラート"),
+          centerTitle: true,
+        ),
+        body: Container(
+          padding: EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Center(
+                  child:
+                      Text("お名前", style: TextStyle(fontSize: 20.0))),
+              TextField(
+                keyboardType: TextInputType.text,
+                style: TextStyle(fontSize: 20.0),
+                controller: null, //TODO
+              ),
+              Image.asset("assets/images/セフトリアキソン.PNG", scale: 10.0,),
+              RaisedButton(
+                onPressed: null, //TODO ボタンのクリック処理
+                child: Text("飲みました！", style: TextStyle(fontSize: 18.0),)
+              )
+            ],
+          ),
+        ));
   }
 }
